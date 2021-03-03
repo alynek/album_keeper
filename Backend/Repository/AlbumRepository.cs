32bb09ab959f9e3f9d1f36cb70be145f21246630
Alyne Gois <alyne0x67@gmail.com>
2021-03-02 21:18:20 -0300

    Adiciona métodos na AlbumController

--- Backend/Repository/AlbumRepository.cs
+++ Backend/Repository/AlbumRepository.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Backend.Model;
+using Microsoft.EntityFrameworkCore;
 
 namespace Backend.Repository
 {
@@ -10,6 +12,21 @@ namespace Backend.Repository
        public AlbumRepository(Context context) : base(context)
        {
            
+       }
+
+       public async Task<IList<Music>> GetMusicFromAlbum(Guid albumId)
+       {
+           return await this.Query.Include(x => x.Musics)
+                .Where(x => x.Id == albumId)
+                .SelectMany(x => x.Musics)
+                .ToListAsync();
+       }
+
+       public async Task<Music> GetMusic(Guid musicId)
+       {
+           return await this.Query.Include(x => x.Musics)
+                .SelectMany(x => x.Musics)
+                .FirstOrDefaultAsync(x => x.Id == musicId);
        }
     }
 }
