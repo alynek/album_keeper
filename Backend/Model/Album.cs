464bae3278efb6c4ed7f31b61d8ab72ef7e9200c
Alyne Gois <alyne0x67@gmail.com>
2021-03-08 23:03:44 -0300

    Adiciona DataAnnotations para validar campos

--- Backend/Model/Album.cs
+++ Backend/Model/Album.cs
@@ -1,14 +1,23 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Backend.Model
 {
     public class Album 
     {
         public Guid Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
+        [Required]
         public string Description { get; set; }
+
+        [Required]
         public string Backdrop { get; set; }
+
+        [Required]
         public string Band { get; set; }
         public IList<Music> Musics { get; set; }
     }
