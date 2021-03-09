e596e76264b68795a0f8c461a5e7359d15f36b6e
Alyne Gois <alyne0x67@gmail.com>
2021-03-08 21:55:55 -0300

    Adiciona métodos à MusicController

--- Backend/Controllers/MusicController.cs
+++ Backend/Controllers/MusicController.cs
@@ -1,7 +1,44 @@
+using Backend.Model;
+using Backend.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
 namespace Backend.Controllers
 {
-    public class MusicController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MusicController : ControllerBase
     {
-        
+        private AlbumRepository AlbumRepository{get; set;}
+
+        public MusicController(AlbumRepository repository)
+        {
+            AlbumRepository = repository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll(Guid id)
+        {
+            var music = await AlbumRepository.GetMusicFromAlbum(id);
+            return Ok(music);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(Guid id)
+        {
+            var music = await AlbumRepository.GetMusic(id);
+            return Ok(music);
+        }
+
+
+        [HttpPost("{albumId}")]
+        public async Task<IActionResult> Post(Guid albumId, Music model)
+        {
+            var album = await AlbumRepository.GetById(albumId);
+            album.Musics.Add(model);
+            await AlbumRepository.Update(album);
+            return Ok();
+        }
     }
 }
